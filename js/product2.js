2f89d1ad0ddfff362324a62d367a235f25171e12
Claudio Paredes <claudioparedesarbelo@gmail.com>
2025-09-30 16:40:46 -0300

    pagina producto

--- js/product2.js
+++ js/product2.js
@@ -2,16 +2,6 @@
         const urlParams = new URLSearchParams(window.location.search);
         const productId = urlParams.get("id");
 
-        async function fetchProductDetails() {
-            try {
-                const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
-                const product = await response.json();
-                displayProductDetails(product);
-            } catch (error) {
-                console.error("Error al obtener detalles del producto:", error);
-                productDetail.innerHTML = `<p class="text-red-500">Error al cargar el producto</p>`;
-            }
-        }
 
         function displayProductDetails(product) {
             productDetail.innerHTML = `
@@ -20,9 +10,14 @@
                 <p class="text-sm text-gray-600 mt-2">Categoría: ${product.category}</p>
                 <p class="text-lg font-bold text-accent2-700 mt-2">$${product.price}</p>
                 <p class="text-sm text-gray-600 mt-2">${product.description}</p>
-                <button class="mt-4 border-2 text-sm p-2 rounded-xl border-accent2-500 bg-accent2-300 hover:bg-accent2-500 text-white transition duration-300">Agregar al carrito</button>
+                <button class="mt-4 border-2 text-sm p-2 rounded-xl border-accent2-500 bg-accent2-300 hover:bg-accent2-500 transition duration-300">Agregar al carrito</button>
             `;
         }
 
-        fetchProductDetails();
+
+fetch(`https://fakestoreapi.com/products/${productId}`)
+    .then(response => response.json())
+    .then(product => displayProductDetails(product));
+
+        
   
